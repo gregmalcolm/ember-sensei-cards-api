10ff0e689dfdf3f8b94853f74357a26020a86b70
gregmalcolm <greg@gregmalcolm.com>
2016-09-04 15:11:35 -0400

    Setup basic project without data

---
+++ README.md
@@ -0,0 +1,37 @@
+Ember Sensei Cards API
+======================
+
+Provided as the backend for Ember Sensei Cards.
+
+Hosts data for
+
+* Sensei Cards
+* Ember Movies
+
+Getting Started
+---------------
+
+1) Install NPM 6.x
+
+It is suggested that you use [Node Version Manager (NVM)](https://github.com/creationix/nvm) to manage your npm installations.
+
+Here is how to install npm 6.5 from the command using nvm:
+
+```
+nvm install 6.5
+nvm alias default 6.5 <-- Optional. Will make npm 6.5 your default npm
+nvm use 6.5
+```
+
+2) Start the server
+
+From the UNIX or Windows command line, cd into this repo and run:
+
+```
+npm start
+
+```
+
+3) Check the browser
+
+The API should be running on `http://localhost:5000`
---
+++ src/index.js
@@ -0,0 +1,42 @@
+'use strict';
+var path = require('path');
+var express = require('express');
+var jsonApi = require('json-api');
+var jsonApiError = jsonApi.types.Error;
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/ember-sensei-cards');
+
+
+var models = {
+  // TODO: implement models
+}
+
+var adapter = new jsonApi.dbAdapters.Mongoose(models);
+var registry = new jsonApi.ResourceTypeRegistry({
+  // TODO: implement resources-descriptions
+}, { dbAdapter: adapter });
+
+var controller = new jsonApi.controllers.API(registry);
+var docs = new jsonApi.controllers.Documentation(registry, {name: 'Ember Sensei Stories API'});
+var app = express();
+var front = new jsonApi.httpStrategies.Express(controller, docs);
+var apiReqHandler = front.apiRequest.bind(front);
+
+// Enable CORS. Note: if you copy this code into production, you may want to
+// disable this. See https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+app.use(function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+})
+
+//routes
+app.get("/", front.docsRequest.bind(front));
+//TODO: Routes
+
+app.use(function(req, res, next) {
+  front.sendError(new jsonApiError(404, undefined, 'Not Found'), req, res);
+});
+
+console.log("Starting an ember-sensei-cards backend server on localhost:5000");
+app.listen(5000);
